cc64399c907cb6f332aa13edc1c4739c045b1b07
Bharati Subramanian <bharatisharada@gmail.com>
2022-02-03 20:38:40 +0530

    feat: hamburger mobile screens
    Added hamburger close and open icon feature in mobile screens.

---
+++ docs-script/hamburger.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector('.hamburger');
+const sidebarClose = document.querySelector('.sidebar__close');
+const sidebar = document.querySelector('.sidebar');
+;
+sidebarClose.addEventListener('click', toggleSidebar);hamburger.addEventListener('click', toggleSidebar);
+
+
+function toggleSidebar(e) {    
+    sidebar.classList.toggle('sidebar--open');
+    sidebar.classList.toggle('sidebar--close');
+}
+
+
